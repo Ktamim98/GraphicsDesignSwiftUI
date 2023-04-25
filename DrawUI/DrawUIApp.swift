ff00116b757f2667185584ca108375768308ab28
Ktamim98 <tamim15-2515@diu.edu.bd>
2023-04-25 21:56:26 +0600

    Initial Commit

---
+++ DrawUI/DrawUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawUIApp.swift
+//  DrawUI
+//
+//  Created by Tamim Khan on 25/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
